1fb26a855c678f52c79c13778a7ea169dc2c64f0
mohsen yousefi <mohsen.nezhadyousefi@gmail.com>
2019-08-01 21:40:05 +0430

    sss

--- app/src/main/java/com/rachcode/peykman/ActivityTransactionDetail.java
+++ app/src/main/java/com/rachcode/peykman/ActivityTransactionDetail.java
@@ -1,6 +1,12 @@
 package com.rachcode.peykman;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,13 +14,23 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.rachcode.peykman.model.ItemHistory;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class ActivityTransactionDetail extends AppCompatActivity {
+public class ActivityTransactionDetail extends AppCompatActivity  implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     @BindView(R.id.driver_name)
     TextView Driver_Name;
     @BindView(R.id.driver_image)
@@ -41,6 +57,7 @@ public class ActivityTransactionDetail extends AppCompatActivity {
     TextView discount;
     @BindView(R.id.textView606)
     TextView finprice;
+    private boolean isMapReady = false;
 
     @BindView(R.id.driver_police_numberA)
     TextView driver_police_numberA;
@@ -54,13 +71,19 @@ public class ActivityTransactionDetail extends AppCompatActivity {
     ConstraintLayout plauqeMashin;
     @BindView(R.id.plauqeMotor)
     ConstraintLayout plauqeMotor;
+    private GoogleMap gMap;
+    ItemHistory itemHistory;
+    private Location lastKnownLocation;
+    private static final int REQUEST_PERMISSION_LOCATION = 99;
+    private LatLng pickUpLatLng;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction_detail);
         ButterKnife.bind(this);
-        ItemHistory itemHistory = (ItemHistory) getIntent().getSerializableExtra("itemHistory");
+        itemHistory = (ItemHistory) getIntent().getSerializableExtra("itemHistory");
         Driver_Name.setText(itemHistory.driver_first_name+" "+itemHistory.driver_last_name);
         Log.i("itemHistory", "onCreate: driver_first_name "+itemHistory.driver_first_name+" driver_last_name"+itemHistory.driver_last_name);
         Glide.with(getApplicationContext()).load(itemHistory.photo).into(driverImage);
@@ -75,6 +98,9 @@ public class ActivityTransactionDetail extends AppCompatActivity {
         price.setText(String.valueOf(itemHistory.price));
         finprice.setText(String.valueOf(itemHistory.final_price));
         discount.setText(String.valueOf(itemHistory.discount_amount));
+
+
+
          switch (itemHistory.order_feature) {
             case "Send-Motor":
                 // set peyk motori
@@ -136,6 +162,179 @@ public class ActivityTransactionDetail extends AppCompatActivity {
                  break;
         }
 
+
+        pickUpLatLng = new LatLng(itemHistory.getStartLatitude(), itemHistory.getStartLongitude());
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gMap = googleMap;
+
+        isMapReady = true;
+        googleMap.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                        this, R.raw.style_map));
+        updateLastLocation(true);
+
+
+        setMarker();
+    }
+
+    private void setMarker() {
+        LatLng latLngPikup = null;
+        LatLng dis1 = null;
+        LatLng dis2 = null;
+        LatLng dis3 = null;
+        LatLng dis4 = null;
+
+        Log.i("www", "Count Marker: "+itemHistory.getdestination_count());
+        switch (itemHistory.getdestination_count()) {
+            case 1:
+                latLngPikup = new LatLng(itemHistory.getStartLatitude(),itemHistory.getStartLongitude());
+                dis1 = new LatLng(itemHistory.getEndLatitude(), itemHistory.getEndLongitude());
+                break;
+
+
+            case 2:
+                latLngPikup = new LatLng(itemHistory.getStartLatitude(),itemHistory.getStartLongitude());
+                dis1 = new LatLng(itemHistory.getEndLatitude(), itemHistory.getEndLongitude());
+                dis2 = new LatLng(itemHistory.getEnd_latitude_second(), itemHistory.getEnd_longitude_second());
+                break;
+
+
+            case 3:
+                latLngPikup = new LatLng(itemHistory.getStartLatitude(),itemHistory.getStartLongitude());
+                dis1 = new LatLng(itemHistory.getEndLatitude(), itemHistory.getEndLongitude());
+                dis2 = new LatLng(itemHistory.getEnd_latitude_second(), itemHistory.getEnd_longitude_second());
+                dis3 = new LatLng(itemHistory.getEnd_latitude_third(), itemHistory.getEnd_longitude_third());
+                break;
+
+
+            case 4:
+                latLngPikup = new LatLng(itemHistory.getStartLatitude(),itemHistory.getStartLongitude());
+                dis1 = new LatLng(itemHistory.getEndLatitude(), itemHistory.getEndLongitude());
+                dis2 = new LatLng(itemHistory.getEnd_latitude_second(), itemHistory.getEnd_longitude_second());
+                dis3 = new LatLng(itemHistory.getEnd_latitude_third(), itemHistory.getEnd_longitude_third());
+                dis4 = new LatLng(itemHistory.getEnd_latitude_fourth(), itemHistory.getEnd_longitude_fourth());
+
+                break;
+        }
+
+        switch (itemHistory.getdestination_count()) {
+            case 0:
+                gMap.addMarker(createMark(0, latLngPikup));
+                gMap.addMarker(createMark(1, dis1));
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLngPikup, 16f));
+                break;
+            case 1:
+                gMap.addMarker(createMark(0, latLngPikup));
+                gMap.addMarker(createMark(1, dis1));
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLngPikup, 16f));
+
+
+
+                break;
+            case 2:
+                gMap.addMarker(createMark(0, latLngPikup));
+                gMap.addMarker(createMark(1, dis1));
+                gMap.addMarker(createMark(2, dis2));
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLngPikup, 16f));
+
+
+                break;
+            case 3:
+                gMap.addMarker(createMark(0, latLngPikup));
+                gMap.addMarker(createMark(2, dis1));
+                gMap.addMarker(createMark(2, dis2));
+                gMap.addMarker(createMark(3, dis3));
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLngPikup, 16f));
+
+
+                break;
+            case 4:
+                gMap.addMarker(createMark(0, latLngPikup));
+                gMap.addMarker(createMark(3, dis1));
+                gMap.addMarker(createMark(3, dis2));
+                gMap.addMarker(createMark(3, dis3));
+                gMap.addMarker(createMark(4, dis4));
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLngPikup, 16f));
+
+                break;
+        }
+    }
+    private void updateLastLocation(boolean move) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_LOCATION);
+            return;
+        }
+        lastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(
+                googleApiClient);
+        gMap.setMyLocationEnabled(true);
+
+        if (pickUpLatLng != null) {
+            if (move) {
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                        pickUpLatLng, 16f)
+                );
+
+                gMap.animateCamera(CameraUpdateFactory.zoomTo(16f));
+            }
+//            fetchNearDriver();
+        }
+    }
+    private MarkerOptions createMark(int positionMarker, final LatLng latLng) {
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        switch (positionMarker) {
+            case 0:
+                markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.icm_pik));
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16f));
+                break;
+
+            case 1:
+                markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.icm_dis));
+                break;
+
+            case 2:
+                markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.icm_dis2));
+                break;
+
+            case 3:
+                markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.icm_dis3));
+                break;
+
+            case 4:
+                markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.icm_dis4));
+                break;
+        }
+        markerOptions.position(latLng);
+
+
+        //         Bitmap bm = ln.getDrawingCache();
+        return markerOptions;
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
--- app/src/main/java/com/rachcode/peykman/model/ItemHistory.java
+++ app/src/main/java/com/rachcode/peykman/model/ItemHistory.java
@@ -22,7 +22,7 @@ public class ItemHistory implements Serializable {
 
     @Expose
     @SerializedName("destination_count")
-    public String destination_count;
+    public int destination_count;
     @Expose
     @SerializedName("discount_amount")
     public String discount_amount;
@@ -131,5 +131,114 @@ public class ItemHistory implements Serializable {
     @SerializedName("image_id")
     public int image_id = R.drawable.ic_mride;
 
+    @SerializedName("end_latitude_second")
+    private double end_latitude_second;
+    @Expose
+    @SerializedName("end_latitude_fourth")
+    private double end_latitude_fourth;
+    @Expose
+    @SerializedName("end_latitude_third")
+    private double end_latitude_third;
+
+    @Expose
+    @SerializedName("end_longitude_second")
+    private double end_longitude_second;
+    @Expose
+    @SerializedName("end_longitude_third")
+    private double end_longitude_third;
+    @Expose
+    @SerializedName("end_longitude_fourth")
+    private double end_longitude_fourth;
+    @Expose
+    @SerializedName("receiver_phone_third")
+    private String receiver_phone_third;
+
+
+    public double getEnd_latitude_second() {
+        return end_latitude_second;
+    }
+
+    public void setEnd_latitude_second(double end_latitude_second) {
+        this.end_latitude_second = end_latitude_second;
+    }
+
+    public double getEnd_latitude_fourth() {
+        return end_latitude_fourth;
+    }
+
+    public void setEnd_latitude_fourth(double end_latitude_fourth) {
+        this.end_latitude_fourth = end_latitude_fourth;
+    }
+
+    public double getEnd_latitude_third() {
+        return end_latitude_third;
+    }
+
+    public void setEnd_latitude_third(double end_latitude_third) {
+        this.end_latitude_third = end_latitude_third;
+    }
+
+    public double getEnd_longitude_second() {
+        return end_longitude_second;
+    }
+
+    public void setEnd_longitude_second(double end_longitude_second) {
+        this.end_longitude_second = end_longitude_second;
+    }
+
+    public double getEnd_longitude_third() {
+        return end_longitude_third;
+    }
+
+    public void setEnd_longitude_third(double end_longitude_third) {
+        this.end_longitude_third = end_longitude_third;
+    }
+
+    public double getEnd_longitude_fourth() {
+        return end_longitude_fourth;
+    }
+
+    public void setEnd_longitude_fourth(double end_longitude_fourth) {
+        this.end_longitude_fourth = end_longitude_fourth;
+    }
+
+    public Double getStartLatitude() {
+        return start_latitude;
+    }
+
+    public void setStartLatitude(Double startLatitude) {
+        this.start_latitude = startLatitude;
+    }
+
+    public Double getStartLongitude() {
+        return start_longitude;
+    }
+
+    public void setStartLongitude(Double startLongitude) {
+        this.start_longitude = startLongitude;
+    }
+
+    public Double getEndLatitude() {
+        return end_latitude;
+    }
+
+    public void setEndLatitude(Double endLatitude) {
+        this.end_latitude = endLatitude;
+    }
+
+    public Double getEndLongitude() {
+        return end_longitude;
+    }
+
+    public void setEndLongitude(Double endLongitude) {
+        this.end_longitude = endLongitude;
+    }
+    public int getdestination_count() {
+        return destination_count;
+    }
+
+    public void setdestination_count(int destination_count) {
+        this.destination_count = destination_count;
+    }
 
 }
